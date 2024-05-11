f37e7978702ad9a03161bcc0157e26209dd20db9
yyypearl <hor629@naver.com>
2024-05-12 02:43:37 +0900

    style(#3): theme, globalStyle 재정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+
 export default function Home() {
-  return <div>Gamegoo Initial settings</div>;
+  return <Example>Gamegoo Initial settings</Example>;
 }
+
+const Example = styled.div`
+  color: ${theme.colors.purple100};
+  ${(props) => props.theme.fonts.bold32};
+`;
