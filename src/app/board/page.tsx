4028af81bbc6bf9d99784db0fd8a3eafe05bab5f
Clara <dhk0729@gmail.com>
2024-05-19 15:51:32 +0900

    file(#15): 게시판 관련 svg 파일 추가

---
+++ src/app/board/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import styled from "styled-components";
+
+const BoardPage = ()=>{
+    return(
+<Wrapper>
+
+</Wrapper> 
+    )
+};
+
+export default BoardPage;
+
+const Wrapper = styled.div`
+    width: 100%;
+`
