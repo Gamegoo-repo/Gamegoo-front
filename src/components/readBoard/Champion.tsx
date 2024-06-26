aaf3b75d40b90f3cf9493c5bbcc291c0c6613538
yyypearl <hor629@naver.com>
2024-06-27 05:18:16 +0900

    design(#28): Champion props 추가

--- src/components/readBoard/Champion.tsx
+++ src/components/readBoard/Champion.tsx
@@ -2,50 +2,53 @@ import styled from "styled-components";
 import Image from "next/image";
 
 interface ChampioinListInterface {
-    id: number;
-    value: string;
+  id: number;
+  value: string;
 }
 
 interface ChampionProps {
-    list: ChampioinListInterface[];
+  list: ChampioinListInterface[];
+  size?: number;
 }
 
 const Champion = (props: ChampionProps) => {
-    const { list } = props;
-    return (
-        <Wrapper>
-            <Title>최근 선호 챔피언</Title>
-            <Images>
-                {list.map((champion) => (
-                    <Image
-                        key={champion.id}
-                        src={champion.value}
-                        width={50}
-                        height={50}
-                        alt="champion image"
-                    />
-                ))}
-            </Images >
-        </Wrapper>
-    )
+  const { list, size = 18 } = props;
+  return (
+    <Wrapper>
+      <Title size={size}>최근 선호 챔피언</Title>
+      <Images>
+        {list.map((champion) => (
+          <Image
+            key={champion.id}
+            src={champion.value}
+            width={50}
+            height={50}
+            alt="champion image"
+          />
+        ))}
+      </Images>
+    </Wrapper>
+  );
 };
 
 export default Champion;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Title = styled.p`
-    ${(props) => props.theme.fonts.semiBold18};
-    color: #222222;
-    margin-bottom:4px;
+const Title = styled.p<{ size: number }>`
+  ${(props) =>
+    props.theme.fonts[
+      `semiBold${props.size}` as keyof typeof props.theme.fonts
+    ]};
+  color: #222222;
+  margin-bottom: ${(props) => (props.size === 14 ? "7px" : "4px")};
 `;
 
 const Images = styled.div`
-    display: flex;
-    align-items: center;
-    gap:9px;
+  display: flex;
+  align-items: center;
+  gap: 9px;
 `;
-
