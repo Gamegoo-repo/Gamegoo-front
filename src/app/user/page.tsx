88f68cbcb58de4e5955947999847b2922e1bf0c6
yyypearl <hor629@naver.com>
2024-06-27 05:22:08 +0900

    design(#28): 유저 프로필 UI

---
+++ src/app/user/page.tsx
@@ -0,0 +1,261 @@
+"use client";
+
+import styled from "styled-components";
+import { useParams, useRouter } from "next/navigation";
+import Profile from "@/components/match/Profile";
+import HeaderTitle from "@/components/common/HeaderTitle";
+import { theme } from "@/styles/theme";
+import { BAD_MANNER_TYPES, MANNER_TYPES } from "@/data/mannerLevel";
+import MannerLevelBar from "@/components/common/MannerLevelBar";
+
+const data = {
+  good_manner: {
+    "1": 8,
+    "2": 5,
+    "3": 2,
+    "4": 5,
+    "5": 0,
+    "6": 5,
+  },
+  bad_manner: {
+    "1": 1,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0,
+    "6": 0,
+  },
+};
+
+const userData = {
+  image: "/assets/images/profile.svg",
+  account: "유니콘의 비밀",
+  tag: "KR1",
+  tier: "B3",
+  manner_level: 5,
+  mic: 0,
+  champions: [
+    { id: 1, value: "/assets/icons/gray_circle.svg" },
+    { id: 2, value: "/assets/icons/gray_circle.svg" },
+    { id: 3, value: "/assets/icons/gray_circle.svg" },
+  ],
+  gameStyle: [
+    "이기기만 하면 뭔들",
+    "과도한 핑은 사절이에요",
+    "랭크 올리고 싶어요",
+  ],
+};
+
+const UserProfile = () => {
+  const router = useRouter();
+  const params = useParams();
+  const id = +params.id;
+
+  const mannerEvaluations = Object.entries(data.good_manner);
+  const badMannerEvaluations = Object.entries(data.bad_manner);
+
+  return (
+    <Wrapper>
+      <MatchContent>
+        <HeaderTitle title="장시은 님의 프로필" size="regular" />
+        <Main>
+          <Profile profileType="other" user={userData} />
+          <Content>
+            <div>
+              <Title>장시은 님의 매너레벨</Title>
+              <Box>
+                <Text>
+                  매너 키워드는 하나 당 1점, 비매너 키워드는 -2점으로 계산해요.
+                  <br />
+                  최근 <Span>4</Span>명의 사용자가 장시은 님에게 긍정적 매너
+                  평가를 남겼어요.
+                </Text>
+                <MannerLevelBar recentLevel={4} percentage={15} />
+              </Box>
+            </div>
+            <div>
+              <Title>매너 키워드</Title>
+              <Box>
+                <MannerList>
+                  <ValueWrapper>
+                    {mannerEvaluations.map(([key, value]) => {
+                      return (
+                        <Value
+                          key={key}
+                          className={value > 0 ? "mannerEmph" : "default"}
+                        >
+                          {value}
+                        </Value>
+                      );
+                    })}
+                  </ValueWrapper>
+                  <TypeWrapper>
+                    {MANNER_TYPES.map((type, index) => {
+                      return (
+                        <Type
+                          key={index}
+                          className={
+                            mannerEvaluations[index][1] > 0
+                              ? "mannerEmph"
+                              : "default"
+                          }
+                        >
+                          {type}
+                        </Type>
+                      );
+                    })}
+                  </TypeWrapper>
+                </MannerList>
+              </Box>
+            </div>
+            <div>
+              <Title>비매너 키워드</Title>
+              <Box>
+                <MannerList>
+                  <ValueWrapper>
+                    {badMannerEvaluations.map(([key, value]) => {
+                      return (
+                        <Value
+                          key={key}
+                          className={value > 0 ? "badEmph" : "default"}
+                        >
+                          {value}
+                        </Value>
+                      );
+                    })}
+                  </ValueWrapper>
+                  <TypeWrapper>
+                    {BAD_MANNER_TYPES.map((type, index) => {
+                      return (
+                        <Type
+                          key={index}
+                          className={
+                            badMannerEvaluations[index][1] > 0
+                              ? "badEmph"
+                              : "default"
+                          }
+                        >
+                          {type}
+                        </Type>
+                      );
+                    })}
+                  </TypeWrapper>
+                </MannerList>
+              </Box>
+            </div>
+          </Content>
+        </Main>
+      </MatchContent>
+    </Wrapper>
+  );
+};
+
+export default UserProfile;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const MatchContent = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  padding: 0 80px;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
+  gap: 14px;
+  margin-bottom: 37px;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 4fr 1fr 1fr;
+  gap: 15px;
+  margin-top: 37px;
+`;
+
+const Title = styled.h1`
+  padding-left: 6px;
+  margin-bottom: 13px;
+  ${(props) => props.theme.fonts.regular25};
+  color: ${theme.colors.gray700};
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 269px;
+  border-radius: 20px;
+  padding: 18px 32px;
+  background: ${theme.colors.gray500};
+`;
+
+const Text = styled.div`
+  ${(props) => props.theme.fonts.regular16};
+  color: ${theme.colors.gray700};
+  margin-top: 8px;
+  margin-bottom: 40px;
+`;
+
+const Span = styled.span`
+  ${(props) => props.theme.fonts.bold16};
+  color: ${theme.colors.purple100};
+`;
+
+const MannerList = styled.div`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+`;
+
+const ValueWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 11px;
+  row-gap: 16px;
+`;
+
+const Value = styled.p`
+  ${(props) => props.theme.fonts.medium16};
+
+  &.default {
+    color: ${theme.colors.gray200};
+  }
+
+  &.mannerEmph {
+    color: ${theme.colors.purple300};
+  }
+
+  &.badEmph {
+    color: ${theme.colors.error200};
+  }
+`;
+
+const TypeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
+
+const Type = styled.p`
+  ${(props) => props.theme.fonts.medium16};
+
+  &.default {
+    color: ${theme.colors.gray200};
+  }
+
+  &.mannerEmph {
+    color: ${theme.colors.purple300};
+  }
+
+  &.badEmph {
+    color: ${theme.colors.error200};
+  }
+`;
--- src/components/match/Profile.tsx
+++ src/components/match/Profile.tsx
@@ -1,68 +1,216 @@
-import React from "react";
+import React, { useState } from "react";
 import { theme } from "@/styles/theme";
 import Image from "next/image";
 import styled from "styled-components";
 import GameStyle from "./GameStyle";
 import { POSITIONS } from "@/data/profile";
+import Champion from "../readBoard/Champion";
+import Toggle from "../common/Toggle";
+import Button from "../common/Button";
+import Report from "../readBoard/Report";
+import FormModal from "../common/FormModal";
+import Checkbox from "../common/Checkbox";
+import { REPORT_REASON } from "@/data/report";
+import Input from "../common/Input";
+import ConfirmModal from "../common/ConfirmModal";
 
-type profileType = "fun" | "hard";
+type profileType = "fun" | "hard" | "other";
+
+interface Champion {
+  id: number;
+  value: string;
+}
+
+interface User {
+  image: string;
+  account: string;
+  tag: string;
+  tier: string;
+  mic: number;
+  champions: Champion[];
+  gameStyle: string[];
+}
 
 interface Profile {
+  user: User;
   profileType: profileType;
 }
 
-const Profile: React.FC<Profile> = ({ profileType = "hard" }) => {
+const Profile: React.FC<Profile> = ({ profileType = "hard", user }) => {
+  const [isMike, setIsMike] = useState(false);
+  const [isMoreBoxOpen, setIsMoreBoxOpen] = useState(false);
+  const [isReportBoxOpen, setIsReportBoxOpen] = useState(false);
+  const [isBlockBoxOpen, setIsBlockBoxOpen] = useState(false);
+  const [isBlockConfirmOpen, setIsBlockConfrimOpen] = useState(false);
+  const [reportDetail, setReportDetail] = useState<string>("");
+
+  const handleMike = () => {
+    setIsMike(!isMike);
+  };
+
+  const handleMoreBoxOpen = () => {
+    setIsMoreBoxOpen((prevState) => !prevState);
+  };
+
+  const handleReport = () => {
+    // 신고하기 api
+    setIsReportBoxOpen(!isReportBoxOpen);
+  };
+
+  const handleBlock = () => {
+    // 차단하기 api
+    setIsBlockBoxOpen(!isBlockBoxOpen);
+  };
+
   return (
-    <Container>
+    <Container className={profileType}>
       <Row>
         <ImageContainer>
-          <Image
-            src="/assets/images/profile.svg"
-            width={186}
-            height={186}
-            alt="프로필"
-          />
-          <CameraImage
-            src="/assets/icons/profile_camera.svg"
-            width={54}
-            height={54}
-            alt="프로필 이미지"
-          />
+          <Image src={user.image} width={186} height={186} alt="프로필" />
+          {profileType !== "other" && (
+            <CameraImage
+              src="/assets/icons/profile_camera.svg"
+              width={54}
+              height={54}
+              alt="프로필 이미지"
+            />
+          )}
         </ImageContainer>
         <StyledBox>
-          <Top>
-            유니콘의 비밀
-            <Span>#KR1</Span>
-            <Rank>
-              <Image
-                src="/assets/images/rank_b3.svg"
-                width={52}
-                height={52}
-                alt="B3"
-              />
-              B3
-            </Rank>
-          </Top>
+          <TopContainer>
+            <Top>
+              {user.account}
+              <Span>{`#${user.tag}`}</Span>
+              <Rank>
+                <Image
+                  src={`/assets/images/rank_${user.tier}.svg`}
+                  width={52}
+                  height={52}
+                  alt="B3"
+                />
+                {user.tier}
+              </Rank>
+            </Top>
+            <More>
+              <Admit>
+                <Button
+                  buttonType="primary"
+                  width="218px"
+                  text="친구 요청 수락하기"
+                />
+                {/* <Button buttonType="secondary" width="218px" text="친구 추가" />
+                <Button
+                  buttonType="secondary"
+                  width="218px"
+                  disabled={true}
+                  text="친구 요청 전송됨"
+                /> */}
+              </Admit>
+              {/* 더보기 버튼 */}
+              <Report onClick={handleMoreBoxOpen} />
+              {isMoreBoxOpen && (
+                <ReportBox>
+                  <ReportText onClick={handleReport}>신고하기</ReportText>
+                  <ReportText onClick={handleBlock}>차단하기</ReportText>
+                </ReportBox>
+              )}
+              {/* 신고하기 팝업 */}
+              {isReportBoxOpen && (
+                <FormModal
+                  type="checkbox"
+                  title="유저 신고하기"
+                  width="494px"
+                  height="721px"
+                  closeButtonWidth={17}
+                  closeButtonHeight={17}
+                  borderRadius="20px"
+                  buttonText="신고하기"
+                  onClose={handleReport}
+                  disabled
+                >
+                  <div>
+                    <ReportLabel>신고 사유</ReportLabel>
+                    <ReportReasonContent>
+                      {REPORT_REASON.map((data) => (
+                        <Checkbox
+                          key={data.id}
+                          value={data.text}
+                          label={data.text}
+                          fontSize="regular18"
+                        />
+                      ))}
+                    </ReportReasonContent>
+                    <ReportLabel>상세 내용</ReportLabel>
+                    <ReportContent>
+                      <Input
+                        inputType="textarea"
+                        value={reportDetail}
+                        onChange={(value) => {
+                          setReportDetail(value);
+                        }}
+                        placeholder="내용을 입력하세요. (선택)"
+                        borderRadius="8px"
+                        fontSize="regular18"
+                        height="134px"
+                      />
+                    </ReportContent>
+                  </div>
+                </FormModal>
+              )}
+              {/* 차단하기 팝업 */}
+              {isBlockBoxOpen && (
+                <ConfirmModal type="yesOrNo" width="540px" onClose={() => {}}>
+                  <Msg>
+                    {`차단한 상대에게는 메시지를 받을 수 없으며\n매칭이 이루어지지 않습니다.\n\n또한, 다시 차단 해제할 수 없습니다.\n차단하시겠습니까?`}
+                  </Msg>
+                </ConfirmModal>
+              )}
+              {/* 차단하기 확인 팝업 */}
+              {isBlockConfirmOpen && (
+                <ConfirmModal
+                  type="confirm"
+                  width="540px"
+                  onClose={() => {
+                    setIsBlockConfrimOpen(false);
+                  }}
+                >
+                  <MsgConfirm>{`차단이 완료되었습니다.`}</MsgConfirm>
+                </ConfirmModal>
+              )}
+            </More>
+          </TopContainer>
           {profileType === "fun" ? (
-            <GameStyle />
+            <GameStyle profileType="none" gameStyle={user.gameStyle} />
           ) : (
-            <Position>
-              {POSITIONS.map((position, index) => (
-                <Posi key={index}>
-                  {position.label}
-                  <Image
-                    src={`/assets/icons/position_${position.position}_purple.svg`}
-                    width={55}
-                    height={44}
-                    alt="포지션"
-                  />
-                </Posi>
-              ))}
-            </Position>
+            <UnderRow>
+              <Position>
+                {(profileType === "other"
+                  ? POSITIONS.slice(0, 2)
+                  : POSITIONS
+                ).map((position, index) => (
+                  <Posi key={index} className={profileType}>
+                    {position.label}
+                    <Image
+                      src={`/assets/icons/position_${position.position}_purple.svg`}
+                      width={55}
+                      height={40}
+                      alt="포지션"
+                    />
+                  </Posi>
+                ))}
+              </Position>
+              <Champion size={14} list={user.champions} />
+              <Mike>
+                마이크
+                <Toggle isOn={isMike} onToggle={handleMike} />
+              </Mike>
+            </UnderRow>
           )}
         </StyledBox>
       </Row>
-      {profileType === "hard" && <GameStyle />}
+      {(profileType === "hard" || "other") && (
+        <GameStyle profileType="other" gameStyle={user.gameStyle} />
+      )}
     </Container>
   );
 };
@@ -71,14 +219,19 @@ export default Profile;
 
 const Container = styled.div`
   width: 100%;
+  box-sizing: border-box;
   border-radius: 30px;
-  padding: 40px 44px;
+  padding: 23px 44px 44px 44px;
   background: ${theme.colors.gray500};
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
   gap: 15px;
+
+  &.other {
+    padding: 39px 44px 48px 44px;
+  }
 `;
 
 const Row = styled.div`
@@ -87,6 +240,10 @@ const Row = styled.div`
   justify-content: flex-start;
   align-items: center;
   gap: 38px;
+`;
+
+const UnderRow = styled(Row)`
+  gap: 54px;
 `;
 
 const ImageContainer = styled.div`
@@ -105,6 +262,16 @@ const StyledBox = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 15px;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 16px;
+  color: ${theme.colors.gray100};
+  font-size: ${theme.fonts.bold32};
 `;
 
 const Top = styled.div`
@@ -129,16 +296,86 @@ const Rank = styled.div`
   font-weight: 300;
 `;
 
+const More = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  margin-bottom: 20px;
+  position: relative;
+`;
+
+const Admit = styled.div``;
+
+const ReportBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 300px;
+  transform: translateY(-50%);
+  z-index: 100;
+  box-shadow: 0 0 21.3px 0 #00000026;
+  background: ${theme.colors.white};
+  padding: 10px 103px 10px 20px;
+  border-radius: 10px;
+`;
+
+const ReportText = styled.p`
+  ${(props) => props.theme.fonts.medium15};
+  color: #606060;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
+const ReportLabel = styled.p`
+  color: ${theme.colors.gray600};
+  ${(props) => props.theme.fonts.semiBold18};
+  margin-bottom: 12px;
+`;
+
+const ReportContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+const ReportReasonContent = styled(ReportContent)`
+  margin-bottom: 38px;
+`;
+
+const Msg = styled.div`
+  text-align: center;
+  color: ${theme.colors.gray600};
+  ${(props) => props.theme.fonts.regular18};
+  margin: 28px 0;
+`;
+
+const MsgConfirm = styled(Msg)`
+  ${(props) => props.theme.fonts.regular25};
+  margin: 80px 0;
+`;
+
 const Position = styled.div`
   display: flex;
   gap: 33px;
   align-items: center;
-  font-size: ${theme.fonts.regular14};
 `;
 
 const Posi = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 10px;
   align-items: center;
+  font-size: ${theme.fonts.regular14};
+
+  &.other {
+    font-size: ${theme.fonts.semiBold14};
+  }
+`;
+
+const Mike = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  font-size: ${theme.fonts.semiBold14};
 `;
