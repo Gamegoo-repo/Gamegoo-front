0448f59c2645d3e77c738e9e744d94c2b510106e
yyypearl <hor629@naver.com>
2024-07-08 17:24:59 +0900

    docs(#31): 마이페이지 tab, dummy data

---
+++ src/data/mypage.ts
@@ -0,0 +1,39 @@
+export const EX_ALARM = [
+    { content: '알림 내용1', time: '1시간', read:true},
+    { content: '알림 내용2', time: '2시간', read:false},
+    { content: '알림 내용3', time: '5시간', read:false},
+    { content: '알림 내용4', time: '10시간', read:true},
+    { content: '알림 내용5', time: '10시간', read:false},
+    { content: '알림 내용6', time: '10시간', read:true},
+    { content: '알림 내용7', time: '10시간', read:true},
+    { content: '알림 내용8', time: '10시간', read:true},
+    { content: '알림 내용9', time: '10시간', read:true},
+];
+
+  
+export const EX_POST = [
+    {
+        profileImg: "",
+        nickname: "유니콘의 비밀",
+        tag: "KR1",
+        tier: "UR",
+        text: "제발 저랑 같이 해주세요 저 잘해요...",
+        time: "24.05.06",
+    },
+    {
+        profileImg: "",
+        nickname: "유니콘의 비밀",
+        tag: "KR1",
+        tier: "B3",
+        text: "텍스트",
+        time: "24.05.06",
+    },
+    {
+        profileImg: "",
+        nickname: "유니콘의 비밀",
+        tag: "KR1",
+        tier: "B3",
+        text: "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무...",
+        time: "24.05.06",
+    },
+];
--- src/data/tab.ts
+++ src/data/tab.ts
@@ -1,5 +1,5 @@
 export const HEADER_MODAL_TAB = [
-    { id: 1, menu: '마이페이지', icon: "setting", url: "/mypage/profile" },
+    { id: 1, menu: '내 정보', icon: "setting", url: "/mypage/profile" },
     { id: 2, menu: '내가 작성한 글', icon: "my_post", url: "/mypage/post" },
     { id: 3, menu: '내 평가', icon: "my_review", url: "/mypage/review" },
     { id: 4, menu: '고객센터', icon: "customer_service", url: "/mypage/service" },
@@ -7,7 +7,7 @@ export const HEADER_MODAL_TAB = [
 ];
 
 export const MYPAGE_TAB = [
-    { id: 1, menu: '마이페이지', url: "/mypage/profile" },
+    { id: 1, menu: '내 정보', url: "/mypage/profile" },
     { id: 2, menu: '알림', url: "/mypage/alert" },
     { id: 3, menu: '내가 작성한 글', url: "/mypage/post" },
     { id: 4, menu: '내 평가', url: "/mypage/review" },
