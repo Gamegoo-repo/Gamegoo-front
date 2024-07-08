711e091a7df96955c44866c8c3163638f2b05471
yyypearl <hor629@naver.com>
2024-07-09 01:28:17 +0900

    fix(#31): build 에러 수정

--- src/app/mypage/review/page.tsx
+++ src/app/mypage/review/page.tsx
@@ -107,7 +107,7 @@ const MyReviewPage = () => {
                               : "default"
                           }
                         >
-                          {type}
+                          {type.text}
                         </Type>
                       );
                     })}
