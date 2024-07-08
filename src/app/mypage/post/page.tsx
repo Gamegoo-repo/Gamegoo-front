2a4f7303b642fea1feb2d870b23fbdb455e24369
yyypearl <hor629@naver.com>
2024-07-08 17:24:20 +0900

    design(#31): 내가 작성한 글 UI

--- src/app/mypage/post/page.tsx
+++ src/app/mypage/post/page.tsx
@@ -2,14 +2,34 @@
 
 import styled from "styled-components";
 import { theme } from "@/styles/theme";
+import Post from "@/components/mypage/post/Post";
+import { EX_POST } from "@/data/mypage";
 
 const MyPostPage = () => {
   return (
     <Wrapper>
       <MyPostContent>
-        <Profile>
+        <PostPage>
           <Title>내가 작성한 글</Title>
-        </Profile>
+          <Columns>
+            <div>소환사명</div>
+            <Center>티어</Center>
+            <Center>메모</Center>
+            <Center>등록일시</Center>
+          </Columns>
+          {EX_POST.map((item, index) => (
+            <Post
+              key={index}
+              index={index + 1}
+              profileImg={item.profileImg}
+              nickname={item.nickname}
+              tag={item.tag}
+              tier={item.tier}
+              text={item.text}
+              time={item.time}
+            />
+          ))}
+        </PostPage>
       </MyPostContent>
     </Wrapper>
   );
@@ -29,7 +49,7 @@ const MyPostContent = styled.div`
   padding: 0 80px;
 `;
 
-const Profile = styled.header`
+const PostPage = styled.header`
   display: flex;
   flex-direction: column;
   align-items: start;
@@ -40,4 +60,21 @@ const Profile = styled.header`
 const Title = styled.h1`
   ${(props) => props.theme.fonts.bold25};
   color: ${theme.colors.gray700};
+  margin-bottom: 39px;
+`;
+
+const Columns = styled.div`
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+  background: ${theme.colors.gray600};
+  color: ${theme.colors.white};
+  ${(props) => props.theme.fonts.bold14};
+  padding: 13px 41px;
+  display: grid;
+  grid-template-columns: 2fr 1fr 2fr 1fr;
+`;
+
+const Center = styled.div`
+  text-align: center;
 `;
