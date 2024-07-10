9ad1e236b762eef4ddbc099baaa89c9a7a2f1a8a
yyypearl <hor629@naver.com>
2024-07-11 01:41:27 +0900

    feat(#41): 프로필 이미지 변경

--- src/components/match/Profile.tsx
+++ src/components/match/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { theme } from "@/styles/theme";
 import Image from "next/image";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import GameStyle from "./GameStyle";
 import { POSITIONS } from "@/data/profile";
 import Champion from "../readBoard/Champion";
@@ -42,7 +42,22 @@ const Profile: React.FC<Profile> = ({ profileType, user }) => {
   const [isReportBoxOpen, setIsReportBoxOpen] = useState(false);
   const [isBlockBoxOpen, setIsBlockBoxOpen] = useState(false);
   const [isBlockConfirmOpen, setIsBlockConfrimOpen] = useState(false);
+  const [isProfileListOpen, setIsProfileListOpen] = useState(false);
   const [reportDetail, setReportDetail] = useState<string>("");
+
+  /* 선택된 현재 프로필 이미지 */
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(
+    parseInt(user.image.slice(-1))
+  );
+
+  /* 프로필 이미지 리스트 중 클릭시*/
+  const handleImageClick = (index: number) => {
+    setSelectedImageIndex(index + 1);
+
+    setTimeout(() => {
+      setIsProfileListOpen(false);
+    }, 300); // 300ms 후에 창이 닫히도록 설정
+  };
 
   useEffect(() => {
     setIsMike(user.mic);
@@ -72,14 +87,47 @@ const Profile: React.FC<Profile> = ({ profileType, user }) => {
     <Container className={profileType}>
       <Row>
         <ImageContainer>
-          <Image src={user.image} width={186} height={186} alt="프로필" />
+          <ProfileImage>
+            <PersonImage
+              src={`/assets/images/profile/profile${selectedImageIndex}.svg`}
+              width={136}
+              height={136}
+              alt="프로필"
+            />
+          </ProfileImage>
           {profileType !== "other" && (
             <CameraImage
               src="/assets/icons/profile_camera.svg"
               width={54}
               height={54}
               alt="프로필 이미지"
+              onClick={() => setIsProfileListOpen(!isProfileListOpen)}
             />
+          )}
+          {/* 프로필 이미지 선택 팝업 */}
+          {isProfileListOpen && (
+            <ProfileListBox>
+              <Image
+                src="/assets/icons/close_white.svg"
+                width={14}
+                height={14}
+                alt="닫기"
+                onClick={() => setIsProfileListOpen(false)}
+              />
+              <ProfileList>
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+                  <ProfileListImage
+                    key={index}
+                    src={`/assets/images/profile/profile${item}.svg`}
+                    width={104}
+                    height={104}
+                    alt="프로필 이미지"
+                    isSelected={index + 1 === selectedImageIndex}
+                    onClick={() => handleImageClick(index)}
+                  />
+                ))}
+              </ProfileList>
+            </ProfileListBox>
           )}
         </ImageContainer>
         <StyledBox>
@@ -250,6 +298,7 @@ export default Profile;
 
 const Container = styled.div`
   width: 100%;
+  min-width: 1000px;
   box-sizing: border-box;
   border-radius: 30px;
   padding: 23px 44px 44px 44px;
@@ -281,10 +330,68 @@ const ImageContainer = styled.div`
   position: relative;
 `;
 
+const ProfileImage = styled.div`
+  width: 186px;
+  height: 186px;
+  border-radius: 93px;
+  background: ${theme.colors.purple300};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PersonImage = styled(Image)`
+  margin-top: 5px;
+  filter: drop-shadow(-4px 10px 10px rgba(63, 53, 78, 0.582));
+`;
+
 const CameraImage = styled(Image)`
   position: absolute;
   bottom: 0px;
   left: 0px;
+`;
+
+const ProfileListBox = styled.div`
+  width: 527px;
+  height: 335px;
+  display: flex;
+  flex-direction: column;
+  padding: 21px;
+  gap: 10px;
+  justify-content: center;
+  align-items: flex-end;
+  border-radius: 20px;
+  background: rgba(0, 0, 0, 0.64);
+  position: fixed;
+  top: 500px;
+  left: 20px;
+  z-index: 100;
+`;
+
+const ProfileList = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 14px 29px 14px;
+  row-gap: 45px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+`;
+
+const ProfileListImage = styled(Image)<{ isSelected: boolean }>`
+  cursor: pointer;
+  transition: opacity 0.3s ease-in-out;
+
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      opacity: 0.5;
+    `}
+
+  &:hover {
+    filter: drop-shadow(0px 4px 10px rgba(138, 117, 255, 0.7));
+    transition: box-shadow 0.3s ease-in-out;
+  }
 `;
 
 const StyledBox = styled.div`
