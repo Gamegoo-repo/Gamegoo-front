1c2641731b7e5601c3778fcb5b462055afbc15b0
Clara <dhk0729@gmail.com>
2024-08-03 00:39:56 +0900

    fix(#54): match data 수정

--- src/data/match.ts
+++ src/data/match.ts
@@ -1,11 +1,11 @@
 export const MATCH_PAGE_DATA = [
-    { id: 1, pathname: 'match', width: '1206px', height: '227px', top: '36px', left: '41px', title: '바로 매칭하기' },
-    { id: 2, pathname: 'board', width: '1206px', height: '227px', top: '36px', left: '41px', title: '매칭 게시판에서 찾기' },
+    { id: 1, pathname: 'match', width: '100%', height: '285px', top: '50%', left: '50%', title: '바로 매칭하기' },
+    { id: 2, pathname: 'board', width: '100%', height: '285px', top: '50%', left: '50%', title: '매칭 게시판에서 찾기' },
 ];
 
 export const MATCH_TYPE_PAGE_DATA = [
-    { id: 1, pathname: 'match', width: '100%', height: '285px', top: '50%', left: '50%', title: '바로 매칭하기' },
-    { id: 2, pathname: 'board', width: '100%', height: '285px', top: '50%', left: '50%', title: '매칭 게시판에서 찾기' },
+    { id: 1, type:'fun',pathname: 'match/game-mode', width: '600px', height: '380px', top: '50%', left: '50%', title: '즐겜' },
+    { id: 2, type:'hard',pathname: 'match/game-mode', width: '600px', height: '380px', top: '50%', left: '50%', title: '빡겜' },
 ];
 
 export const GAME_MODE_PAGE_DATA = [
