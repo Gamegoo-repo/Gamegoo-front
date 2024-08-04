0f3bc33c1dd8621a99f251145ed8b31b72714aaf
yyypearl <hor629@naver.com>
2024-08-05 04:00:54 +0900

    feat(#49): 마이페이지 API 함수 선언

---
+++ src/api/mypage.ts
@@ -0,0 +1,62 @@
+import { AuthAxios } from ".";
+
+export const getProfile= async () => {
+  const endpoint = '/v1/member/profile';
+  try {
+    const response = await AuthAxios.get(endpoint);
+    console.log("유저 프로필 조회 성공:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("유저 프로필 조회 실패:", error);
+    throw error;
+  }
+};
+
+export const putProfileImg= async (profileImage: string) => {
+  const endpoint = '/v1/member/profile_image';
+  try {
+    console.log("이거바", profileImage);
+    const response = await AuthAxios.put(endpoint, { profileImage });
+    console.log("프로필 이미지 수정 성공:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("프로필 이미지 수정 실패:", error);
+    throw error;
+  }
+};
+
+export const putGameStyle = async (gameStyleIdList: string[]) => {
+  const endpoint = '/v1/member/gamestyle';
+  try {
+    const response = await AuthAxios.put(endpoint, { gameStyleIdList });
+    console.log("게임스타일 수정 성공:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("게임스타일 수정 실패:", error);
+    throw error;
+  }
+};
+
+export const putPosition = async ({ minP, subP }: { minP: string, subP: string }) => {
+  const endpoint = '/v1/member/position';
+  try {
+    const response = await AuthAxios.put(endpoint, { minP, subP});
+    console.log("포지션 수정 성공:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("포지션 수정 실패:", error);
+    throw error;
+  }
+};
+
+export const deleteMember = async () => {
+    const endpoint = '/v1/member';
+    try {
+      const response = await AuthAxios.post(endpoint);
+      console.log('회원탈퇴 성공:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('회원탈퇴 실패:', error);
+      throw error;
+    }
+  };
