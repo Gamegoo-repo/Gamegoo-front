de90a48d0c55093f7586ea515627546fed5fbaad
yyypearl <hor629@naver.com>
2024-08-13 01:40:24 +0900

    fix(#49): GameStyle 필드명 변경

--- src/components/readBoard/GameStyle.tsx
+++ src/components/readBoard/GameStyle.tsx
@@ -3,39 +3,38 @@ import { theme } from "@/styles/theme";
 import { GAME_STYLE } from "@/data/profile";
 
 interface GameStyleProps {
-    styles: number[];
+  styles: number[];
 }
 
 const GameStyle = (props: GameStyleProps) => {
-    const { styles } = props;
+  const { styles } = props;
 
-    const getTextById = (styleId: number) => {
-        const gameStyle = GAME_STYLE.find(style => style.id === styleId);
-        return gameStyle ? gameStyle.text : '';
-    };
+  const getTextById = (styleId: number) => {
+    const gameStyle = GAME_STYLE.find((style) => style.gameStyleId === styleId);
+    return gameStyle ? gameStyle.gameStyleName : "";
+  };
 
-    return (
-        <Div>
-            {styles && styles.length > 0 &&
-                styles.map((data, index) => (
-                    <Content key={index}>
-                        {getTextById(data)}
-                    </Content>
-                ))}
-        </Div>
-    )
+  return (
+    <Div>
+      {styles &&
+        styles.length > 0 &&
+        styles.map((data, index) => (
+          <Content key={index}>{getTextById(data)}</Content>
+        ))}
+    </Div>
+  );
 };
 
 export default GameStyle;
 
 const Div = styled.div`
-    display: grid;
-    grid-gap: 11px;
-    grid-template-columns: repeat(3, minmax(100px, auto));
+  display: grid;
+  grid-gap: 11px;
+  grid-template-columns: repeat(3, minmax(100px, auto));
 `;
 
 const Content = styled.p`
-  padding:6px 21px;  
+  padding: 6px 21px;
   background: ${theme.colors.purple100};
   color: ${theme.colors.white};
   ${(props) => props.theme.fonts.medium14};
