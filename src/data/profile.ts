8dacb968fd22c57594718841a2d17cd194cd4fd4
yyypearl <hor629@naver.com>
2024-08-12 15:03:54 +0900

    docs(#49): 게임스타일 데이터 내용 변경

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -5,22 +5,23 @@ export const EX_GAME_STYLE = [
 ];
 
 export const GAME_STYLE = [
-    { gameStyleId: 1, gameStyleName: '원챔러' },
-    { gameStyleId: 2, gameStyleName: '광물 탈출' },
-    { gameStyleId: 3, gameStyleName: '랭크 올리고 싶어요' },
-    { gameStyleId: 4, gameStyleName: '이기기만 하면 뭔들' },
-    { gameStyleId: 5, gameStyleName: '바른말 사용' },
-    { gameStyleId: 6, gameStyleName: '답장 빨라요' },
-    { gameStyleId: 7, gameStyleName: '마이크 필수' },
+    { gameStyleId: 1, gameStyleName: '광물 탈출' },
+    { gameStyleId: 2, gameStyleName: '랭크 올리고 싶어요' },
+    { gameStyleId: 3, gameStyleName: '이기기만 하면 뭔들' },
+    { gameStyleId: 4, gameStyleName: '바른말 사용' },
+    { gameStyleId: 5, gameStyleName: '답장 빨라요' },
+    { gameStyleId: 6, gameStyleName: '마이크 필수' },
+    { gameStyleId: 7, gameStyleName: '마이크 안해요' },
     { gameStyleId: 8, gameStyleName: '과도한 핑은 사절이에요'},
     { gameStyleId: 9, gameStyleName: '즐겜러' },
-    { gameStyleId: 10, gameStyleName: '원챔이에요' },
-    { gameStyleId: 11, gameStyleName: '욕하면 던질거예요' },
-    { gameStyleId: 12, gameStyleName: '뚝심있는 탑 라이너' },
-    { gameStyleId: 13, gameStyleName: '프로 정글러' },
-    { gameStyleId: 14, gameStyleName: '칼바람 장인' },
-    { gameStyleId: 15, gameStyleName: '뚝심있는 탑 라이너' },
-    { gameStyleId: 16, gameStyleName: '마이크 안해요' },
+    { gameStyleId: 10, gameStyleName: '빡겜러' },
+    { gameStyleId: 11, gameStyleName: '원챔러' },
+    { gameStyleId: 12, gameStyleName: '욕하지 말아요' },
+    { gameStyleId: 13, gameStyleName: '뚝심있는 탑' },
+    { gameStyleId: 14, gameStyleName: '갱킹마스터 정글러' },
+    { gameStyleId: 15, gameStyleName: '1인군단 원딜러' },
+    { gameStyleId: 16, gameStyleName: '무한 백업 서포터' },
+    { gameStyleId: 17, gameStyleName: '칼바람 장인' },
 ];
 
 export const POSITIONS = [
