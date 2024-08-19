770e6458fd5be8b4fa786d8de75ffa8471f8901c
yyypearl <hor629@naver.com>
2024-08-19 23:54:12 +0900

    feat(#63): 알림 관련 API 함수 정의

---
+++ src/api/notification.ts
@@ -0,0 +1,53 @@
+import { AuthAxios } from "./auth";
+
+/* 알림 전체 목록 조회 */
+export const getNotiTotal = async (page: number) => {
+    const endpoint = `/v1/notification/total?page=${page}`;
+    try {
+      const response = await AuthAxios.get(endpoint);
+      console.log("알림 전체 목록 조회 성공:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("알림 전체 목록 조회 실패:", error);
+      throw error;
+    }
+  };
+
+/* 알림 읽음 처리 */
+export const readNoti = async (notificationId:number) => {
+    const endpoint = `/v1/notification/${notificationId}`;
+    try {
+      const response = await AuthAxios.patch(endpoint);
+      console.log("알림 읽음 처리 성공:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("알림 읽음 처리 실패:", error);
+      throw error;
+    }
+  };
+
+/* 안 읽은 알림 개수 조회 */
+export const getNotiCount = async () => {
+    const endpoint = '/v1/notification/unread/count';
+    try {
+      const response = await AuthAxios.get(endpoint);
+      console.log("안 읽은 알림 개수 조회 성공:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("안 읽은 알림 개수 조회 실패:", error);
+      throw error;
+    }
+  };
+
+/* 알림 모달 목록 조회 (cusor) */
+export const getNotiModal = async () => {
+    const endpoint = '/v1/notification';
+    try {
+      const response = await AuthAxios.get(endpoint);
+      console.log("알림 모달 목록 조회 성공:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("알림 모달 목록 조회 실패:", error);
+      throw error;
+    }
+  };
