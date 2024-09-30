ae11135e410eb677ec02a20a93cb7c3cac021f88
yyypearl <hor629@naver.com>
2024-09-30 21:54:30 +0900

    style(#81): middleware 토큰 관련 코드 주석 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getAccessToken } from '@/utils/storage';
 
 export function middleware(request: NextRequest) {
-  const token = getAccessToken();
+  const token = request.headers.get('Authorization')?.split(' ')[1];
   const { pathname } = request.nextUrl;
 
   // 로그인하지 않은 상태
-  if (!token && (pathname.startsWith('/match') || pathname.startsWith('/matching')) || pathname.startsWith('/mypage') || pathname.startsWith('/user')) {
+  if (false && (pathname.startsWith('/match') || pathname.startsWith('/matching') || pathname.startsWith('/mypage') || pathname.startsWith('/user'))) {
+  // if (token && (pathname.startsWith('/match') || pathname.startsWith('/matching') || pathname.startsWith('/mypage') || pathname.startsWith('/user'))) {
     // 루트 경로로 리다이렉트
     return NextResponse.redirect(new URL('/', request.url));
   }
