9242efbf73ea25dbf4055d47c9d78028fc905985
yyypearl <hor629@naver.com>
2024-09-30 14:11:40 +0900

    fix(#81): 소켓 중복 연결 오류 안되게 수정

--- src/socket.ts
+++ src/socket.ts
@@ -7,25 +7,32 @@ let socket: Socket | null = null;
 let socketId: string | null = null;
 
 export const connectSocket = (): void => {
-  const token = sessionStorage.getItem('accessToken');
+  if (socket) {
+    console.log("이미 소켓이 연결되어 있습니다.");
+    return;
+  }
 
-  const options = token ? { auth: { token } } : {};
+  // 클라이언트 사이드에서만 실행
+  if (typeof window !== "undefined") {
+    const token = sessionStorage.getItem("accessToken");
+    const options = token ? { auth: { token } } : {};
 
-  socket = io(SOCKET_URL, options);
-  // socket = io(SOCKET_URL);
-  socket.on("connect", () => {
-    console.log("서버 연결. Socket ID:", socket?.id);
-    socketId = socket?.id || null;
-    localStorage.setItem('gamegooSocketId', socketId || '');
-  });
+    socket = io(SOCKET_URL, options);
 
-  socket.on("disconnect", () => {
-    console.log("서버 연결 끊김");
-    localStorage.removeItem('gamegooSocketId');
-    socketId = null;
-  });
+    socket.on("connect", () => {
+      console.log("서버 연결. Socket ID:", socket?.id);
+      socketId = socket?.id || null;
+      localStorage.setItem("gamegooSocketId", socketId || "");
+    });
 
-  setupSocketListeners();
+    socket.on("disconnect", () => {
+      console.log("서버 연결 끊김");
+      localStorage.removeItem("gamegooSocketId");
+      socketId = null;
+    });
+
+    setupSocketListeners();
+  }
 };
 
 export const sendMatchingQuitEvent = (): void => {
