b63bbef2f15bb89cbe55881f1c6abe5b8fd35cbd
yyypearl <hor629@naver.com>
2024-10-04 16:45:10 +0900

    feat(#69): 로그인 유무 액세스 토큰으로 판별 변경

--- src/components/common/ProtectRoute.tsx
+++ src/components/common/ProtectRoute.tsx
@@ -1,23 +1,18 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import { RootState } from "@/redux/store";
 import { getAccessToken } from "@/utils/storage";
 
 const ProtectRoute: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const isLoggedIn = useSelector((state: RootState) => !!state.user.gameName); // gameName이 있을 때 로그인으로 간주 (id 대신)
+  const accesssToken = getAccessToken(); // 로그인 유무 결정
   const router = useRouter();
 
   useEffect(() => {
-    const token = getAccessToken();
-
-    // 로그인 상태가 아니거나 토큰이 없으면 리다이렉트
-    if (!isLoggedIn || !token) {
+    if (!accesssToken) {
       router.push("/"); // "/"으로 리다이렉트
     }
-  }, [isLoggedIn, router]);
+  }, [accesssToken, router]);
 
   return <>{children}</>;
 };
