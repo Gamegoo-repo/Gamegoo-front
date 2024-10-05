8bb58b37e398651562ee4c6cc2d2ad0229db2beb
yyypearl <hor629@naver.com>
2024-10-06 03:02:35 +0900

    fix(#97): 매칭 게시판 유저 티어, 태그, 랭크 정보 안 나오는 오류 수정

--- src/components/crBoard/User.tsx
+++ src/components/crBoard/User.tsx
@@ -4,68 +4,72 @@ import Image from "next/image";
 import { setAbbrevTier } from "@/utils/custom";
 
 interface UserProps {
-    account: string;
-    tag: string;
-    tier: string;
-    rank: number;
+  account: string;
+  tag: string;
+  tier: string;
+  rank: number;
 }
 
 const User = (props: UserProps) => {
-    const { account, tag, tier, rank } = props;
+  const { account, tag, tier, rank } = props;
 
-    return (
-        <Wrapper>
-            <UserAccount>
-                <Account>{account}</Account>
-                {tag &&
-                    <Tag>#{tag}</Tag>
-                }
-            </UserAccount>
-            <UserTier>
-                {tier &&
-                    <>
-                        <Image
-                            src={!tier ? "/assets/images/tier/UNRANK.svg" : `/assets/images/tier/${tier}.svg`}
-                            width={32}
-                            height={20}
-                            alt="티어 이미지"
-                        />
-                        <Tier>{!tier ? "UR" : setAbbrevTier(tier)}{rank}</Tier>
-                    </>
-                }
-            </UserTier>
-        </Wrapper>
-    )
-
+  return (
+    <Wrapper>
+      <UserAccount>
+        <Account>{account}</Account>
+        {tag && <Tag>#{tag}</Tag>}
+      </UserAccount>
+      <UserTier>
+        {tier && (
+          <>
+            <Image
+              src={
+                !tier
+                  ? "/assets/images/tier/UNRANK.svg"
+                  : `/assets/images/tier/${tier}.svg`
+              }
+              width={32}
+              height={20}
+              alt="티어 이미지"
+            />
+            <Tier>
+              {!tier ? "UR" : setAbbrevTier(tier)}
+              {rank}
+            </Tier>
+          </>
+        )}
+      </UserTier>
+    </Wrapper>
+  );
 };
 
 export default User;
 
-const Wrapper = styled.div``
+const Wrapper = styled.div``;
 
 const UserAccount = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const Account = styled.p`
-    ${(props) => props.theme.fonts.bold20};
-    color:${theme.colors.black};
+  ${(props) => props.theme.fonts.bold20};
+  color: ${theme.colors.black};
 `;
 
 const Tag = styled.p`
-    ${(props) => props.theme.fonts.regular20};
-    color:${theme.colors.gray300};
-    margin-left: 5px;
+  ${(props) => props.theme.fonts.regular20};
+  color: ${theme.colors.gray300};
+  margin-left: 5px;
 `;
 
 const UserTier = styled.div`
-    display: flex;
-    align-items: center;
-    gap:2px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
 `;
 
 const Tier = styled.p`
-    ${(props) => props.theme.fonts.regular18};
-    color:#44515C;
+  ${(props) => props.theme.fonts.regular18};
+  color: #44515c;
 `;
