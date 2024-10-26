86a5d828daff5850c13990af45cb7c0ba07bdeea
yyypearl <hor629@naver.com>
2024-10-27 03:34:04 +0900

    fix(#97): 게시판 글 내용 max-height 일 때를 고려해 모달 크기 변경

--- src/components/crBoard/CRModal.tsx
+++ src/components/crBoard/CRModal.tsx
@@ -73,9 +73,9 @@ const Wrapper = styled.div<{ $type: string }>`
   max-width: 555px;
   width: 100%;
   position: relative;
-  min-height: ${({ $type }) => ($type === "posting" ? "837px" : "880px")};
-  max-height:  ${({ $type }) => ($type === "posting" ? "837px" : "880px")};
-  height: 100%;
+  min-height: ${({ $type }) => ($type === "posting" ? "837px" : "1000px")};
+  max-height: ${({ $type }) => ($type === "posting" ? "837px" : "1000px")};
+  height: auto;
   margin: 50px;
   padding: 0 20px;
 
