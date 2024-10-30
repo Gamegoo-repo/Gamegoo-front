ef3a0dce5f6cd7fa6baad4dd3f3033afbfd14f4c
yyypearl <hor629@naver.com>
2024-10-30 18:52:10 +0900

    feat(#147): 파비콘 link 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,24 +21,6 @@ import {
 } from "@/utils/storage";
 import { notify } from "@/hooks/notify";
 
-export const metadata = {
-  title: "롤 실시간 듀오 매칭 - GAMEGOO",
-  titleTemplate: "%s - 레터링",
-  description:
-    "리그 오브 레전드 유저를 위한 실시간 듀오 매칭 서비스입니다. 듀오 찾기 게시판, 실시간 채팅, 친구 추가 기능을 통해 원하는 게임 친구를 바로 찾아보세요.",
-  icons: {
-    icon: "/icon.png",
-  },
-  openGraph: {
-    site_name: "겜구(Gamegoo)",
-    title: "롤 실시간 듀오 매칭 - GAMEGOO",
-    description:
-      "리그 오브 레전드 유저를 위한 실시간 듀오 매칭 서비스입니다. 듀오 찾기 게시판, 실시간 채팅, 친구 추가 기능을 통해 원하는 게임 친구를 바로 찾아보세요.",
-    url: "https://www.gamegoo.co.kr",
-    type: "website",
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -110,6 +92,7 @@ export default function RootLayout({
           content="리그 오브 레전드 유저를 위한 실시간 듀오 매칭 서비스입니다.
 듀오 찾기 게시판, 실시간 채팅, 친구 추가 기능을 통해 원하는 게임 친구를 바로 찾아보세요."
         />
+        <link rel="icon" href="/icon.png" />
       </head>
       <body>
         <HelmetProvider>
