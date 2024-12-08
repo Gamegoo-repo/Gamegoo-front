a9ed82d80a9720fcc5b2626b3be2d2211e851740
yyypearl <hor629@naver.com>
2024-12-08 16:18:43 +0900

    fix(#158): 토큰 만료 관련 이벤트 수신 시 리프레시 토큰 재발급해서 전달

--- src/hooks/useJwtError.ts
+++ src/hooks/useJwtError.ts
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { connectSocket, socket } from '@/socket';
-import { getAccessToken } from '@/utils/storage';
-import { isTokenExpired } from '@/utils/auth';
-import { AuthAxios, reissueToken } from '@/api/auth';
+import { reissueToken } from '@/api/auth';
 
 const useJwtError = () => {
 
@@ -12,42 +10,12 @@ const useJwtError = () => {
             connectSocket();
         }
 
-        AuthAxios.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                const { response: { status } } = error;
-
-                if (status === 401) {
-                    try {
-                        const response = await reissueToken();
-                        const newToken = response.result.refreshToken;
-
-                        // 재발급된 토큰을 로컬 저장소에 저장
-                        if (localStorage.getItem('accessToken')) {
-                            localStorage.setItem('accessToken', newToken);
-                        } else {
-                            sessionStorage.setItem('accessToken', newToken);
-                        }
-
-                        // 요청을 새로운 토큰으로 다시 전송
-                        error.config.headers['Authorization'] = `Bearer ${newToken}`;
-                        return AuthAxios(error.config);
-                    } catch (tokenReissueError) {
-                        console.error("토큰 재발급 실패:", tokenReissueError);
-                        // 재발급 실패 시 추가 처리 (로그아웃 등)
-                        throw tokenReissueError;
-                    }
-                }
-
-                return Promise.reject(error);
-            }
-        );
-
         const handleJwtExpiredError = async (res: any) => {
             const { eventName, eventData } = res.data;
 
             try {
-                const newToken = getAccessToken();
+                const response = await reissueToken();
+                const newToken = response.result.refreshToken;
                 socket?.emit(eventName, { ...eventData, token: newToken });
             } catch (error) {
                 console.error("소켓 이벤트 전송 실패:", error);
@@ -56,7 +24,8 @@ const useJwtError = () => {
 
         const handleConnectionJwtError = async () => {
             try {
-                const newToken = getAccessToken();
+                const response = await reissueToken();
+                const newToken = response.result.refreshToken;
                 socket?.emit("connection-update-token", { token: newToken });
             } catch (error) {
                 console.error("연결 업데이트 실패:", error);
