94d8c7fdef0a576a6656b04b917a3fc0f5508364
yyypearl <hor629@naver.com>
2025-01-11 19:57:18 +0900

    refactor(#168): 채팅창 Tab과 Header 분리 리팩토링

---
+++ src/components/chat/Tabs.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
+import { Dispatch } from "react";
+
+interface TabsProps {
+  tabs: string[];
+  activeTab: number;
+  onTabClick: Dispatch<React.SetStateAction<number>>;
+}
+
+const Tabs = (props: TabsProps) => {
+  const { tabs, activeTab, onTabClick } = props;
+  return (
+    <TabContainer>
+      {tabs.map((tab, index) => (
+        <TabButton
+          key={index}
+          onClick={() => onTabClick(index)}
+          $isActive={activeTab === index}
+        >
+          {tab}
+        </TabButton>
+      ))}
+    </TabContainer>
+  );
+};
+
+export default Tabs;
+
+const TabContainer = styled.div`
+  display: flex;
+  gap: 40px;
+  padding: 0 30px;
+`;
+
+const TabButton = styled.div<{ $isActive: boolean }>`
+  position: relative;
+  padding: 4px 0;
+  cursor: pointer;
+  ${(props) =>
+    props.$isActive ? props.theme.fonts.bold14 : props.theme.fonts.semiBold14};
+  color: ${theme.colors.gray600};
+  &:after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    bottom: -2px;
+    width: ${(props) => (props.$isActive ? "100%" : "none")};
+    height: 4px;
+    background-color: ${theme.colors.purple100};
+    border-radius: 60px;
+    transform: translateX(-50%);
+    transition: width 0.3s ease;
+  }
+`;
