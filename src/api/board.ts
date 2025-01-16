bd87eb8182fff502e8ab02b0b1ab3760606abff1
Yong Hyeok Lee <129850728+HelloWorldDogs223@users.noreply.github.com>
2025-01-16 11:05:46 +0900

    feat(#171): v2 연결 다시

--- src/api/board.ts
+++ src/api/board.ts
@@ -17,7 +17,7 @@ interface ListInterface {
 /* 글쓰기 */
 export const postBoard = async (params: PostReq): Promise<PostsResponse> => {
   try {
-    const response = await AuthAxios.post("/v1/posts", params);
+    const response = await AuthAxios.post("/api/v2/posts", params);
     return response.data;
   } catch (error) {
     console.error("글쓰기 실패:", error);
@@ -30,7 +30,7 @@ export const getBoardList = async (
   params: ListInterface
 ): Promise<GetBoardListResponse> => {
   try {
-    const response = await AuthAxios.get("/v1/posts/list", { params });
+    const response = await AuthAxios.get("/api/v2/posts/list", { params });
     return response.data;
   } catch (error) {
     console.error("게시판 목록 불러오기 실패:", error);
@@ -43,7 +43,7 @@ export const getMemberPost = async (
   postId: number
 ): Promise<MemberPostBoardResponse> => {
   try {
-    const response = await AuthAxios.get(`/v1/posts/member/list/${postId}`);
+    const response = await AuthAxios.get(`/api/v2/posts/member/list/${postId}`);
     return response.data;
   } catch (error) {
     console.error("로그인 상태 게시글 조회 실패:", error);
@@ -56,7 +56,7 @@ export const getNonMemberPost = async (
   postId: number
 ): Promise<NotMemberBoardResponse> => {
   try {
-    const response = await Axios.get(`/v1/posts/list/${postId}`);
+    const response = await Axios.get(`/api/v2/posts/list/${postId}`);
     return response.data;
   } catch (error) {
     console.error("로그아웃 상태 게시글 조회 실패:", error);
@@ -70,7 +70,7 @@ export const editPost = async (
   params: PostReq
 ): Promise<BoardEditResponse> => {
   try {
-    const response = await AuthAxios.put(`/v1/posts/${postId}`, params);
+    const response = await AuthAxios.put(`/api/v2/posts/${postId}`, params);
     return response.data;
   } catch (error) {
     console.error("게시글 수정 실패:", error);
@@ -83,7 +83,7 @@ export const deletePost = async (
   postId: number
 ): Promise<BoardDeleteResponse> => {
   try {
-    const response = await AuthAxios.delete(`/v1/posts/${postId}`);
+    const response = await AuthAxios.delete(`/api/v2/posts/${postId}`);
     return response.data;
   } catch (error) {
     console.error("게시글 삭제 실패:", error);
