2e4c61d725c7b03f8fdc6321d2dd17413df4f519
yyypearl <hor629@naver.com>
2025-02-08 02:40:57 +0900

    feat(#184): 대화방 내에서도 채팅창 위치 이동하도록 수정

--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -56,10 +56,11 @@ interface System {
 
 interface ChatLayoutProps {
   apiType: number;
+  onDragStart: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 const ChatLayout = (props: ChatLayoutProps) => {
-  const { apiType } = props;
+  const { apiType, onDragStart } = props;
   const dispatch = useDispatch();
 
   const [isMoreBoxOpen, setIsMoreBoxOpen] = useState(false);
@@ -611,12 +612,14 @@ const ChatLayout = (props: ChatLayoutProps) => {
       <Overlay>
         {isChatRoomOpen && chatEnterData && isChatUuid !== null && (
           <Wrapper onClick={handleOutsideModalClick}>
-            <MessageHeader
-              isMoreBoxOpen={isMoreBoxOpen}
-              chatEnterData={chatEnterData}
-              onMoreBoxOpen={handleMoreBoxOpen}
-              menuItems={menuItems}
-            />
+            <HeaderWrapper onMouseDown={onDragStart}>
+              <MessageHeader
+                isMoreBoxOpen={isMoreBoxOpen}
+                chatEnterData={chatEnterData}
+                onMoreBoxOpen={handleMoreBoxOpen}
+                menuItems={menuItems}
+              />
+            </HeaderWrapper>
             <MessageList
               chatEnterData={chatEnterData}
               systemMessage={systemMessage}
@@ -834,13 +837,7 @@ const ChatLayout = (props: ChatLayoutProps) => {
 
 export default ChatLayout;
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 50%;
-  right: 8%;
-  transform: translate(0, -50%);
-  z-index: 1;
-`;
+const Overlay = styled.div``;
 
 const Wrapper = styled.div`
   position: relative;
@@ -848,7 +845,12 @@ const Wrapper = styled.div`
   border-radius: 20px;
   display: flex;
   flex-direction: column;
-  width: 418px;
+  width: 100%;
+`;
+
+const HeaderWrapper = styled.div`
+  user-select: auto;
+  cursor: move;
 `;
 
 const CheckContent = styled.div`
