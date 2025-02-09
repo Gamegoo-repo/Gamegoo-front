fced8bca3c925b46bd435c013a836ed375404b73
yyypearl <hor629@naver.com>
2025-02-10 01:27:04 +0900

    docs(#182): ux 라이팅 변경사항 적용

--- src/data/terms.tsx
+++ src/data/terms.tsx
@@ -2,27 +2,6 @@ import { theme } from "@/styles/theme";
 import React from "react";
 import styled from "styled-components";
 
-const Text = styled.div`
-  color: #7c7c7c;
-  ${theme.fonts.regular16};
-  white-space: nowrap;
-`;
-
-const Required = styled.span`
-  color: ${theme.colors.error100};
-  margin-right: 6px;
-`;
-
-const Span = styled.span`
-  color: ${theme.colors.purple100};
-  transition: color 200ms, text-decoration 500ms;
-
-  &:hover {
-    color: ${theme.colors.purple200};
-    text-decoration: underline;
-  }
-`;
-
 export const createTerms = (
   openModal: (type: string, index: number) => void
 ) => [
@@ -30,9 +9,8 @@ export const createTerms = (
     id: 1,
     text: (
       <Text>
-        <Required>*</Required>
-        (필수){` `}
-        <Span onClick={() => openModal("SERVICE", 0)}>이용약관</Span>에
+        <Required $isRequired={true}>(필수)</Required>
+        <Span onClick={() => openModal("SERVICE", 0)}>이용 약관</Span>에
         동의합니다.
       </Text>
     ),
@@ -42,12 +20,9 @@ export const createTerms = (
     id: 2,
     text: (
       <Text>
-        <Required>*</Required>
-        (필수){` `}
-        <Span onClick={() => openModal("PRIVATE", 1)}>
-          개인정보 수집 및 이용
-        </Span>
-        에 동의합니다.
+        <Required $isRequired={true}>(필수)</Required>
+        <Span onClick={() => openModal("PRIVATE", 1)}>개인정보 처리방침</Span>에
+        동의합니다.
       </Text>
     ),
     require: true,
@@ -56,7 +31,7 @@ export const createTerms = (
     id: 3,
     text: (
       <Text>
-        (선택){` `}
+        <Required $isRequired={false}>(선택)</Required>
         서비스 홍보 및 마케팅 목적의{` `}
         <Span onClick={() => openModal("MARKETING", 2)}>
           개인정보 수집 및 이용
@@ -67,3 +42,25 @@ export const createTerms = (
     require: false,
   },
 ];
+
+const Text = styled.div`
+  color: ${theme.colors.gray700};
+  ${theme.fonts.regular16};
+  white-space: nowrap;
+`;
+
+const Required = styled.span<{ $isRequired: boolean }>`
+  color: ${({ $isRequired }) =>
+    $isRequired ? theme.colors.violet800 : theme.colors.gray500};
+  ${theme.fonts.regular16};
+  margin-right: 4px;
+`;
+
+const Span = styled.span`
+  text-decoration: underline;
+  transition: color 200ms, text-decoration 500ms;
+
+  &:hover {
+    color: ${theme.colors.violet600};
+  }
+`;
