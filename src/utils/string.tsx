125aa1df24db7b8d359e01ce7a6471ea83f5d419
yyypearl <hor629@naver.com>
2025-02-15 16:57:33 +0900

    feat(#196): 첫 자만 대문자, 그 외 소문자로 변환하는 함수 추가

--- src/utils/string.tsx
+++ src/utils/string.tsx
@@ -1,3 +1,8 @@
 export function toLowerCaseString(input: string) {
   if (input) return input.toLowerCase();
 }
+
+export function toCapitalizedString(input: string) {
+  if (!input) return input;
+  return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+}
