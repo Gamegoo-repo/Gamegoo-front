ac8bdb56c9b66a88bbea3e134d3383ab7ab8223c
rowanna <rowankimna@gmail.com>
2025-03-07 17:33:20 +0900

    style(#211): header 반응형 작업 완료 - chatButton MO용 따로 작업 - Footer, Header에 chat icon 반응형에 따라 적용 - Header css grid로 동일 마크업에 MO만 다른 스타일 적용 - useMediaQueries 훅 추가 - theme에 semiBold10 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -204,17 +204,9 @@ const fonts = {
     weight: 400,
     size: 14,
   }),
-  regular13: FONT({
-    weight: 400,
-    size: 13,
-  }),
   regular12: FONT({
     weight: 400,
     size: 12,
-  }),
-  regular9: FONT({
-    weight: 400,
-    size: 9,
   }),
   regular8: FONT({
     weight: 400,
