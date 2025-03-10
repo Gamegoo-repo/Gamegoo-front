04984401f818501fa9479f663238baba064c983d
yyypearl <hor629@naver.com>
2025-03-10 13:16:32 +0900

    fix(#206): 채팅 즐겨찾기 및 해제 API 연결 오류 수정

--- src/components/chat/Layout.tsx
+++ src/components/chat/Layout.tsx
@@ -190,30 +190,41 @@ const Layout = () => {
   ) => {
     event.stopPropagation();
 
-    // friends 배열과 검색된 친구 목록에서 해당 친구 찾기
-    const friend = friends.find((f) => f.memberId === friendId);
-    if (friend) {
-      const newLikedStatus = !friend.liked;
-
+    try {
+      const response = await patchFriendStar(friendId);
+      console.log(response.data.friendMemberId);
+      const updatedFriendId = response.data.friendMemberId;
       // friends 상태 업데이트
       setFriends((prevFriends) =>
         prevFriends.map((f) =>
-          f.memberId === friendId ? { ...f, isLiked: newLikedStatus } : f
+          f.memberId === updatedFriendId ? { ...f, liked: !f.liked } : f
         )
       );
 
       // favoriteFriends 상태 업데이트
-      setFavoriteFriends((prevFavorites) =>
-        newLikedStatus
-          ? [...prevFavorites, { ...friend, isLiked: newLikedStatus }]
-          : prevFavorites.filter((f) => f.memberId !== friendId)
-      );
+      setFavoriteFriends((prevFavorites) => {
+        const isCurrentlyFavorite = prevFavorites.some(
+          (f) => f.memberId === updatedFriendId
+        );
 
-      try {
-        await patchFriendStar(friendId);
-      } catch (error) {
-        console.error(error);
-      }
+        if (isCurrentlyFavorite) {
+          // 이미 즐겨찾기 상태면 제거
+          return prevFavorites.filter((f) => f.memberId !== updatedFriendId);
+        } else {
+          // 즐겨찾기에 추가
+          const updatedFriend = friends.find(
+            (f) => f.memberId === updatedFriendId
+          );
+          return updatedFriend
+            ? [
+                ...prevFavorites,
+                { ...updatedFriend, liked: !updatedFriend.liked },
+              ]
+            : prevFavorites;
+        }
+      });
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -753,7 +764,7 @@ const Header = styled.div`
 
 const HeaderTitle = styled.p`
   ${(props) => props.theme.fonts.bold20};
-  color: ${theme.colors.gray600};
+  color: ${theme.colors.gray800};
 `;
 
 const CloseButton = styled.button`
@@ -771,7 +782,6 @@ const ChatMain = styled.div`
   background: ${theme.colors.white};
   &.friend {
     box-shadow: none;
-    padding-right: 6px;
   }
   &.chat {
     box-shadow: inset 0 0 4.7px 0 #00000026;
