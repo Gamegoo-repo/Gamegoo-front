fa36c07f04cd308c1d627b4e1b63e92bb7aef1e4
rowanna <rowankimna@gmail.com>
2025-04-06 20:29:37 +0900

    style(#211): 게임 모드 선택 반응형 작업

--- src/app/match/game-mode/page.tsx
+++ src/app/match/game-mode/page.tsx
@@ -7,8 +7,11 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { GAME_MODE_PAGE_DATA } from "@/constants/match";
 import HeaderTitle from "@/components/common/HeaderTitle";
 import { Suspense } from "react";
+import useMediaQueries from "@/hooks/useMediaQueries";
+import ChevronRight from "../../../../public/assets/icons/chevron_right.svg";
 
 const GameModePage = () => {
+  const isMobile = useMediaQueries({ breakpoint: 700 });
   const router = useRouter();
   const [displayedData, setDisplayedData] = useState(GAME_MODE_PAGE_DATA);
   const searchParams = useSearchParams();
@@ -22,23 +25,46 @@ const GameModePage = () => {
       <MatchContent>
         <HeaderTitle title="게임모드 선택" />
         <Main>
-          {filteredData.map((box) => {
-            return (
-              <BoxWrapper key={box.id}>
-                <GraphicBox
-                  type={params || ""}
-                  rank={box.rank}
-                  pathname={box.pathname}
-                  height={box.height}
-                  top={box.top}
-                  left={box.left}
-                  backgroundColor="#2E3032"
-                >
-                  {box.title}
-                </GraphicBox>
-              </BoxWrapper>
-            );
-          })}
+          {isMobile
+            ? filteredData.map((box) => {
+                const type = params || "";
+                return (
+                  <Box key={box.id} backgroundColor="#2E3032">
+                    <BoxTitle>{box.title}</BoxTitle>
+                    <BoxButton
+                      onClick={() =>
+                        router.push(
+                          type
+                            ? box.rank
+                              ? `${box.pathname}?type=${type}&rank=${box.rank}`
+                              : `${box.pathname}?type=${type}`
+                            : box.pathname
+                        )
+                      }
+                    >
+                      선택
+                      <ChevronRight />
+                    </BoxButton>
+                  </Box>
+                );
+              })
+            : filteredData.map((box) => {
+                return (
+                  <BoxWrapper key={box.id}>
+                    <GraphicBox
+                      type={params || ""}
+                      rank={box.rank}
+                      pathname={box.pathname}
+                      height={box.height}
+                      top={box.top}
+                      left={box.left}
+                      backgroundColor="#2E3032"
+                    >
+                      {box.title}
+                    </GraphicBox>
+                  </BoxWrapper>
+                );
+              })}
         </Main>
       </MatchContent>
     </Wrapper>
@@ -58,12 +84,18 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 110px;
+  @media (max-width: 700px) {
+    padding-top: 0px;
+  }
 `;
 
 const MatchContent = styled.div`
   max-width: 1440px;
   width: 100%;
   padding: 60px 80px 0px 80px;
+  @media (max-width: 700px) {
+    padding: 24px 20px;
+  }
 `;
 
 const Main = styled.main`
@@ -73,6 +105,36 @@ const Main = styled.main`
   gap: 27px;
   margin-top: 185px;
   margin-bottom: 65px;
+  @media (max-width: 700px) {
+    flex-direction: column;
+    margin-top: 24px;
+  }
+`;
+const Box = styled.div<{ backgroundColor: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 89px;
+  padding: 36px 33px;
+  border-radius: 12px;
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.theme.colors.white};
+`;
+
+const BoxTitle = styled.div`
+  ${(props) => props.theme.fonts.bold20};
+`;
+
+const BoxButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 67px;
+  height: 33px;
+  border-radius: 9999px;
+  color: ${(props) => props.theme.colors.white};
+  background: ${(props) => props.theme.colors.violet600};
 `;
 
 const BoxWrapper = styled.div`
