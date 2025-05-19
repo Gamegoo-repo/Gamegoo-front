7826f369c1ec2d4e00543197084d35bcc13465cc
rowanna <rowankimna@gmail.com>
2025-05-19 20:06:44 +0900

    style(#249): 알림 반응형 적용

--- src/constants/tab.ts
+++ src/constants/tab.ts
@@ -1,18 +1,18 @@
 export const HEADER_MODAL_TAB = [
-    { id: 1, menu: '내 정보', icon: "setting", url: "/mypage/profile" },
-    { id: 2, menu: '내가 작성한 글', icon: "my_post", url: "/mypage/post" },
-    { id: 3, menu: '내 평가', icon: "my_review", url: "/mypage/review" },
-    { id: 4, menu: '고객센터', icon: "customer_service", url: "/mypage/service" },
-    // { id: 5, menu: '도움말', icon: "help", url: "/mypage/help" },
-    { id: 6, menu: '로그아웃', icon: "logout", url: "/login" },
+  { id: 1, menu: "마이페이지", icon: "setting", url: "/mypage/profile" },
+  { id: 2, menu: "내가 작성한 글", icon: "my_post", url: "/mypage/post" },
+  { id: 3, menu: "내 평가", icon: "my_review", url: "/mypage/review" },
+  { id: 4, menu: "고객센터", icon: "customer_service", url: "/mypage/service" },
+  // { id: 5, menu: '도움말', icon: "help", url: "/mypage/help" },
+  { id: 6, menu: "로그아웃", icon: "logout", url: "/login" },
 ];
 
 export const MYPAGE_TAB = [
-    { id: 1, menu: '내 정보', url: "/mypage/profile" },
-    { id: 2, menu: '알림', url: "/mypage/notification" },
-    { id: 3, menu: '내가 작성한 글', url: "/mypage/post" },
-    { id: 4, menu: '내 평가', url: "/mypage/review" },
-    { id: 5, menu: '차단목록', url: "/mypage/blocked" },
-    { id: 6, menu: '고객센터', url: "/mypage/service" },
-    // { id: 7, menu: '도움말', url: "/mypage/help" },
+  { id: 1, menu: "내 정보", url: "/mypage/profile" },
+  { id: 2, menu: "알림", url: "/mypage/notification" },
+  { id: 3, menu: "내가 작성한 글", url: "/mypage/post" },
+  { id: 4, menu: "내 평가", url: "/mypage/review" },
+  { id: 5, menu: "차단목록", url: "/mypage/blocked" },
+  { id: 6, menu: "고객센터", url: "/mypage/service" },
+  // { id: 7, menu: '도움말', url: "/mypage/help" },
 ];
