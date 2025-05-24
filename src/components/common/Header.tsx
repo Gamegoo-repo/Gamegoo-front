4ce04b3ecce0b2d4d49c4517f75ed9da1e58e683
rowanna <rowankimna@gmail.com>
2025-05-24 16:18:16 +0900

    style(#249): header pc 로그인 버튼 변경, 마이페이지 내정보로 워딩 통일

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -435,9 +435,16 @@ const HeaderProfileImg = styled.object`
 `;
 
 const Login = styled.button`
-  color: ${theme.colors.violet600};
-  ${(props) => props.theme.fonts.bold14}
+  background: ${theme.colors.violet600};
+  color: ${theme.colors.white};
+  padding: 8px 24px;
+  border-radius: 6px;
+  ${(props) => props.theme.fonts.bold18}
   @media screen and (max-width: 700px) {
+    background: none;
+    color: ${theme.colors.violet600};
+    ${(props) => props.theme.fonts.bold14}
+    padding: 0;
     margin-right: 20px;
     grid-column: 2;
     grid-row: 1;
--- src/constants/tab.ts
+++ src/constants/tab.ts
@@ -1,5 +1,5 @@
 export const HEADER_MODAL_TAB = [
-  { id: 1, menu: "마이페이지", icon: "setting", url: "/mypage/profile" },
+  { id: 1, menu: "내 정보", icon: "setting", url: "/mypage/profile" },
   { id: 2, menu: "내가 작성한 글", icon: "my_post", url: "/mypage/post" },
   { id: 3, menu: "내 평가", icon: "my_review", url: "/mypage/review" },
   { id: 4, menu: "고객센터", icon: "customer_service", url: "/mypage/service" },
