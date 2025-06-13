af982a5830e0572da3a20811eec34d13c4e53530
yyypearl <hor629@naver.com>
2025-06-14 04:46:34 +0900

    design(#259): 프로필 이미지 수정 아이콘 pencil 변경 및 위치 조정

--- src/components/createBoard/UpdateProfileImage.tsx
+++ src/components/createBoard/UpdateProfileImage.tsx
@@ -26,20 +26,14 @@ const UpdateProfileImage = (props: FileInputProps) => {
         <ImageWrapper $bgColor={getProfileBgColor(selectedImageIndex)}>
           <ProfileImage
             data={`/assets/images/profile/profile${selectedImageIndex}.svg`}
-            width={isMobile ? 38 : 62}
-            height={isMobile ? 38 : 60}
+            width={52}
+            height={52}
           />
         </ImageWrapper>
       )}
-      <Label htmlFor="profileImg">
-        <CameraImgBg onClick={() => setIsProfileListOpen(!isProfileListOpen)}>
-          <CameraImage
-            data="/assets/icons/edit_pencil.svg"
-            width={13}
-            height={10}
-          />
-        </CameraImgBg>
-      </Label>
+      <CameraImgBg onClick={() => setIsProfileListOpen(!isProfileListOpen)}>
+        <CameraImage data="/assets/icons/pencil.svg" width={26} height={26} />
+      </CameraImgBg>
       {/* 프로필 이미지 선택 팝업 */}
       {isProfileListOpen && (
         <ProfileListBox>
@@ -114,14 +108,10 @@ const ProfileImage = styled.object`
   pointer-events: none;
 `;
 
-const Label = styled.label`
-  cursor: pointer;
-`;
-
 const CameraImgBg = styled.div`
   position: relative;
-  width: 23px;
-  height: 23px;
+  width: 26px;
+  height: 26px;
   background: #000000a1;
   box-shadow: 0 0 3.06px 0 #00000040;
   border-radius: 50%;
@@ -153,7 +143,7 @@ const ProfileListBox = styled.div`
   align-items: flex-end;
   border-radius: 13px;
   background: rgba(0, 0, 0, 0.64);
-  top: 164px;
+  top: 134px;
   left: 253px;
 `;
 
