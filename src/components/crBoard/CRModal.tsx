5addce2fd1264558dedff15de1392989595aa32c
yyypearl <hor629@naver.com>
2025-06-14 04:46:55 +0900

    design(#259): 글 작성 화면 CRModal 높이 수정

--- src/components/crBoard/CRModal.tsx
+++ src/components/crBoard/CRModal.tsx
@@ -75,8 +75,8 @@ const Wrapper = styled.div<{
   max-width: 580px;
   width: 100%;
   position: relative;
-  min-height: ${({ $type }) => ($type === "posting" ? "837px" : "1000px")};
-  max-height: ${({ $type }) => ($type === "posting" ? "837px" : "1000px")};
+  min-height: ${({ $type }) => ($type === "posting" ? "910px" : "1000px")};
+  max-height: ${({ $type }) => ($type === "posting" ? "910px" : "1000px")};
   height: auto;
   margin: 50px;
   padding: 48px 32px 32px 32px;
