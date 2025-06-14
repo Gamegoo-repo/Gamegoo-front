21b9c8e36767056a968e5da2d314867b2f096f31
rowanna <rowankimna@gmail.com>
2025-06-15 00:14:07 +0900

    design(#253): 게시판 상세 아이디, 태그 글자수 초과 시 말풍선 작업

--- src/components/crBoard/UserAccount.tsx
+++ src/components/crBoard/UserAccount.tsx
@@ -3,6 +3,8 @@ import { theme } from "@/styles/theme";
 import Mic from "../common/Mic";
 import { Mike } from "@/types/user/mike";
 import { useRouter } from "next/navigation";
+import useMediaQueries from "@/hooks/useMediaQueries";
+import { useState } from "react";
 
 interface UserAccountProps {
   account: string;
@@ -14,19 +16,58 @@ interface UserAccountProps {
 const UserAccount = (props: UserAccountProps) => {
   const { account, memberId, mike, tag } = props;
   const router = useRouter();
+  const isMobile = useMediaQueries({ breakpoint: 700 });
+  const [isAccountTouch, setIsAccountTouch] = useState(false);
+  const [isTagTouch, setIsTagTouch] = useState(false);
 
   const handleRouteProfile = () => {
     if (memberId) {
       router.push(`/user/${memberId}`);
     }
   };
+  const formatTextOverNumber = (text: string, maxLength: number) => {
+    if (text.length >= maxLength) {
+      return text.slice(0, maxLength) + "...";
+    }
+    return text;
+  };
   return (
     <Wrapper>
       <Row>
-        <Account onClick={handleRouteProfile}>{account}</Account>
+        <Account onClick={handleRouteProfile}>
+          {isMobile && account.length >= 14 ? (
+            <Text
+              onMouseDown={() => {
+                setIsAccountTouch((prev) => !prev);
+                setIsTagTouch(false);
+              }}
+            >
+              {formatTextOverNumber(account, 14)}
+              {isAccountTouch && <TextModal>{account}</TextModal>}
+            </Text>
+          ) : (
+            account
+          )}
+        </Account>
         {mike && <Mic status={mike} />}
       </Row>
-      {tag && <Tag>#{tag}</Tag>}
+      {tag && (
+        <Tag>
+          {isMobile && tag.length >= 20 ? (
+            <Text
+              onMouseDown={() => {
+                setIsTagTouch((prev) => !prev);
+                setIsAccountTouch(false);
+              }}
+            >
+              {`#${formatTextOverNumber(tag, 20)}`}
+              {isTagTouch && <TextModal>{tag}</TextModal>}
+            </Text>
+          ) : (
+            <>{`#${tag}`}</>
+          )}
+        </Tag>
+      )}
     </Wrapper>
   );
 };
@@ -50,6 +91,38 @@ const Account = styled.button`
 
   @media (max-width: 700px) {
     ${(props) => props.theme.fonts.bold16};
+  }
+`;
+
+const Text = styled.p`
+  position: relative;
+`;
+
+const TextModal = styled.div`
+  color: ${theme.colors.white};
+  ${(props) => props.theme.fonts.semiBold14};
+  position: absolute;
+  z-index: 1;
+  top: 100%;
+  left: 0;
+  border-radius: 10px;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.7);
+  /* Background Blur */
+  box-shadow: 0 4px 8.9px 0 rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(7.5px);
+
+  &::after {
+    /* tail css */
+    content: "";
+    position: absolute;
+    top: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+    border-top: 0 solid transparent;
+    border-left: 4.5px solid transparent;
+    border-right: 4.5px solid transparent;
+    border-bottom: 10px solid rgba(0, 0, 0, 0.64);
   }
 `;
 
