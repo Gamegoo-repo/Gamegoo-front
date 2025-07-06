23390cdc6800f59a901491290ba14854674a7ff2
yyypearl <hor629@naver.com>
2025-07-07 03:44:18 +0900

    design(#301): 포지션 박스 반응형 수정 및 useTooptipTransformOffset 재적용

--- src/components/common/Champion.tsx
+++ src/components/common/Champion.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Image from "next/image";
 import { theme } from "@/styles/theme";
-import { useState } from "react";
-import { fadeIn, fadeOut } from "@/styles/animation";
+import { useEffect, useRef, useState } from "react";
 import { ChampionResponseDTO } from "@/types/api/champion/champion";
 import useMediaQueries from "@/hooks/useMediaQueries";
+import useTooltipTransformOffset from "@/hooks/useTooltipTransformOffset";
 
 interface ChampionProps {
   title?: boolean;
@@ -17,6 +17,35 @@ const Champion = (props: ChampionProps) => {
   const { list, font = "semiBold18", color, title = false } = props;
   const isMobile = useMediaQueries({ breakpoint: 700 });
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  const tooltipWrapperRef = useRef<HTMLDivElement>(null);
+  const { tooltipRef, offsetX, updateOffset } = useTooltipTransformOffset();
+
+  useEffect(() => {
+    if (!isMobile) return;
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        tooltipWrapperRef.current &&
+        tooltipWrapperRef.current.contains(e.target as Node)
+      ) {
+        return; // 내부 클릭이면 무시
+      }
+      setHoveredIndex(null);
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, [isMobile]);
+
+  const handleMouseEnter = (key: number, e: React.MouseEvent) => {
+    setHoveredIndex(key);
+
+    const rect = (e.currentTarget as HTMLElement).getBoundingClientRect();
+    updateOffset(rect);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
 
   // 챔피언 이미지를 로드 실패 시 기본 이미지로
   const handleImageError = (e: any) => {
@@ -35,8 +64,8 @@ const Champion = (props: ChampionProps) => {
           {list?.map((champion, key) => (
             <ChampionWrapper
               key={key}
-              onMouseEnter={() => setHoveredIndex(key)}
-              onMouseLeave={() => setHoveredIndex(null)}
+              onMouseEnter={(e) => handleMouseEnter(key, e)}
+              onMouseLeave={handleMouseLeave}
             >
               <ImageWrapper>
                 <Image
@@ -53,49 +82,59 @@ const Champion = (props: ChampionProps) => {
                 <Percentage>{Math.round(champion.winRate)}%</Percentage>
               </ImageWrapper>
               {hoveredIndex === key && (
-                <Tooltip>
-                  <ChampionName>
-                    <strong>{champion.championName}</strong>
-                  </ChampionName>
-                  <ChampionTable>
-                    <Head>승률</Head>
-                    <Rate>{Math.round(champion.winRate)}%</Rate>
-                    <More>
-                      {champion.wins || "0"}승{" "}
-                      {champion.wins ? champion.games - champion.wins : "0"}패
-                    </More>
-                    <Head>KDA</Head>
-                    <Rate>
-                      {champion.kda ? Number(champion.kda).toFixed(2) : "0.00"}
-                    </Rate>
-                    <More>
-                      {`${
-                        champion.kills
-                          ? Number(champion.kills).toFixed(1)
-                          : "0.0"
-                      } / ${
-                        champion.deaths
-                          ? Number(champion.deaths).toFixed(1)
-                          : "0.0"
-                      } / ${
-                        champion.assists
-                          ? Number(champion.assists).toFixed(1)
-                          : "0.0"
-                      }`}
-                    </More>
-                    <Head>CS</Head>
-                    <Rate>
-                      {champion.csPerMinute !== undefined
-                        ? champion.csPerMinute.toFixed(1)
-                        : "-"}
-                    </Rate>
-                    <More>
-                      {champion.averageCs !== undefined
-                        ? champion.averageCs.toFixed(1)
-                        : "-"}
-                    </More>
-                  </ChampionTable>
-                </Tooltip>
+                <TooltipWrapper ref={tooltipWrapperRef}>
+                  <TooltipTail />
+                  <Tooltip
+                    ref={tooltipRef}
+                    style={{
+                      transform: `translateX(calc(-50% + ${offsetX}px))`,
+                    }}
+                  >
+                    <ChampionName>
+                      <strong>{champion.championName}</strong>
+                    </ChampionName>
+                    <ChampionTable>
+                      <Head>승률</Head>
+                      <Rate>{Math.round(champion.winRate)}%</Rate>
+                      <More>
+                        {champion.wins || "0"}승{" "}
+                        {champion.wins ? champion.games - champion.wins : "0"}패
+                      </More>
+                      <Head>KDA</Head>
+                      <Rate>
+                        {champion.kda
+                          ? Number(champion.kda).toFixed(2)
+                          : "0.00"}
+                      </Rate>
+                      <More>
+                        {`${
+                          champion.kills
+                            ? Number(champion.kills).toFixed(1)
+                            : "0.0"
+                        } / ${
+                          champion.deaths
+                            ? Number(champion.deaths).toFixed(1)
+                            : "0.0"
+                        } / ${
+                          champion.assists
+                            ? Number(champion.assists).toFixed(1)
+                            : "0.0"
+                        }`}
+                      </More>
+                      <Head>CS</Head>
+                      <Rate>
+                        {champion.csPerMinute !== undefined
+                          ? champion.csPerMinute.toFixed(1)
+                          : "-"}
+                      </Rate>
+                      <More>
+                        {champion.averageCs !== undefined
+                          ? champion.averageCs.toFixed(1)
+                          : "-"}
+                      </More>
+                    </ChampionTable>
+                  </Tooltip>
+                </TooltipWrapper>
               )}
             </ChampionWrapper>
           ))}
@@ -172,12 +211,20 @@ const Percentage = styled.div`
   }
 `;
 
+const TooltipWrapper = styled.div`
+  position: absolute;
+  z-index: 9999;
+  top: 40px;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 const Tooltip = styled.div`
   position: absolute;
-  bottom: -170px;
+  bottom: -190px;
   left: 50%;
-  transform: translateX(-50%);
-  width: 223px;
   height: 154px;
   display: flex;
   flex-direction: column;
@@ -187,31 +234,43 @@ const Tooltip = styled.div`
   color: ${theme.colors.white};
   border-radius: 14px;
   ${theme.fonts.regular14};
-  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(5.3px);
-  z-index: 10;
+  pointer-events: none;
   text-align: left;
-  animation: ${fadeIn} 0.3s ease-in-out;
+  z-index: 9999;
 
-  &::before {
-    content: "";
-    position: absolute;
-    top: -10.5px;
-    left: 50%;
-    transform: translateX(-50%);
-    border-width: 6px;
-    border-style: solid;
-    border-color: transparent transparent rgba(0, 0, 0, 0.7) transparent;
+  @media (max-width: 700px) {
+    height: 150px;
+    bottom: -165px;
+    border-radius: 10px;
+    gap: 8px;
   }
+`;
 
-  ${ChampionWrapper}:not(:hover) & {
-    animation: ${fadeOut} 0.3s ease-in-out;
+const TooltipTail = styled.div`
+  position: absolute;
+  bottom: -36px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 0;
+  height: 0;
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-bottom: 6px solid rgba(0, 0, 0, 0.7);
+
+  @media (max-width: 700px) {
+    bottom: -16px;
   }
 `;
 
 const ChampionName = styled.div`
   color: ${theme.colors.gray100};
   ${theme.fonts.regular18};
+
+  @media (max-width: 700px) {
+    ${theme.fonts.regular16};
+  }
 `;
 
 const ChampionTable = styled.div`
@@ -219,7 +278,8 @@ const ChampionTable = styled.div`
   grid-template-columns: 1fr 1fr 2fr;
   grid-template-rows: repeat(3, 1fr);
   row-gap: 6px;
-  column-gap: 20px;
+  column-gap: 10px;
+  white-space: nowrap;
 `;
 
 const Head = styled.div`
@@ -230,6 +290,9 @@ const Head = styled.div`
 const Rate = styled.div`
   color: ${theme.colors.gray100};
   ${theme.fonts.bold14};
+  @media (max-width: 700px) {
+    ${theme.fonts.semiBold14};
+  }
 `;
 
 const More = styled.div`
