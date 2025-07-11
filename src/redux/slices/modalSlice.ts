5dbed0dacbc3753852d389818c08cc4be6352b30
rowanna <rowankimna@gmail.com>
2025-07-11 15:28:23 +0900

    fix(#318): 게시판 팝업 스크롤 막는 작업

--- src/redux/slices/modalSlice.ts
+++ src/redux/slices/modalSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { lockBodyScroll, unlockBodyScroll } from "@/utils";
 
 interface ModalState {
   isOpen: boolean;
@@ -23,36 +24,46 @@ const modalSlice = createSlice({
     /* 매너,비매너 선택 모달 */
     setOpenMannerStatusModal: (state) => {
       state.isOpen = true;
+      lockBodyScroll();
     },
     setCloseMannerStatusModal: (state) => {
       state.isOpen = false;
+      unlockBodyScroll();
     },
     /* 매너,비매너 평가하기 모달 */
     setOpenEvaluationModal: (state) => {
       state.evaluationModal = true;
+      lockBodyScroll();
     },
     setCloseEvaluationModal: (state) => {
       state.evaluationModal = false;
+      unlockBodyScroll();
     },
     setOpenModal: (state, action) => {
       state.modalType = action.payload;
+      lockBodyScroll();
     },
     setCloseModal: (state) => {
       state.modalType = "";
+      unlockBodyScroll();
     },
     /* 게시판 읽기 모달 */
     setOpenReadingModal: (state) => {
       state.readingModal = true;
+      lockBodyScroll();
     },
     setCloseReadingModal: (state) => {
       state.readingModal = false;
+      unlockBodyScroll();
     },
     /* 게시판 쓰기 모달 */
     setOpenPostingModal: (state) => {
       state.postingModal = true;
+      lockBodyScroll();
     },
     setClosePostingModal: (state) => {
       state.postingModal = false;
+      unlockBodyScroll();
     },
   },
 });
