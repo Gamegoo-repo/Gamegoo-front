eb0dcd77542b6f268781132a50d990c4ebd3958f
rowanna <rowankimna@gmail.com>
2025-07-12 01:08:43 +0900

    fix(#320): 내정보, 로그인페이지 모바일 가로모드 잘림현상 개선

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -261,6 +261,7 @@ const Header = () => {
       )}
       {isMyPage && (
         <MyPageModal ref={myPageRef}>
+          <Background>
           {isMobile && (
             <MyPageModalHeader>
               <MyPageModalHeaderTitle>내정보</MyPageModalHeaderTitle>
@@ -337,6 +338,7 @@ const Header = () => {
               </TabItemWrapper>
             ))}
           </TabMenu>
+          </Background>
         </MyPageModal>
       )}
     </Head>
@@ -496,7 +498,6 @@ const MyPageModal = styled.div`
     right: 0;
     bottom: 0;
     width: 100vw;
-    height: 100vh;
     box-shadow: none;
   }
 `;
@@ -545,6 +546,26 @@ const MyName = styled.div`
   white-space: nowrap;
 `;
 
+const Background = styled.div`
+  overflow-y: auto;
+
+  /* 스크롤바 */
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 66px;
+    background: ${theme.colors.gray500};
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 66px;
+    background: transparent;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobile}) {
+    height: calc(100vh - 64px);
+  }
+`;
 const TabMenu = styled.div`
   display: flex;
   flex-direction: column;
