8c743ee380865ad39cd5e2928c0993054c3bb4d9
조하은 <jhe93@naver.com>
2025-08-01 16:21:59 +0900

    add(#364): 포지션 아이콘 경로 저장

---
+++ public/assets/icons/board/positionIcons.ts
@@ -0,0 +1,9 @@
+// assets/positionIcons.ts
+export const POSITION_ICON = {
+  ALL: "/assets/images/position/default/default_all.svg",
+  TOP: "/assets/images/position/default/default_top.svg",
+  JUNGLE: "/assets/images/position/default/default_jungle.svg",
+  MID: "/assets/images/position/default/default_mid.svg",
+  ADC: "/assets/images/position/default/default_one_deal.svg",
+  SUPPORT: "/assets/images/position/default/default_supporter.svg",
+};
